04da6f7072e3fba737e5060aaa53990b04054dec
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-01-30 11:58:31 -0300

    primeiros passos na linguagem R

---
+++ 01-RStudio.R
@@ -0,0 +1,14 @@
+# Nome dos Contributors
+contributors()
+
+# Licença de Uso
+licence()
+
+# Informações sobre a sessão
+sessionInfo()
+
+# Imprimir na tela
+print('Estou iniciando minha caminhada na carreira Cientista de Dados')
+
+# Criar gráficos
+plot(1:25)
