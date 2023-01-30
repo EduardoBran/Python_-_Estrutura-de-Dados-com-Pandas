2b46d8ec0e40a6679b0ea0c2ef56e774e2489d59
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-01-30 12:40:24 -0300

    obter HELP na linguagem R

--- 01-RStudio.R
+++ 01-RStudio.R
@@ -24,3 +24,30 @@ library(ggplot2)
 
 # Descarregar o pacote
 detach(package:ggplot2)
+
+# Sabendo o nome da função, vamos descobrir o que faz a função mean
+# mean é uma função para calcular a média aritmética
+help(mean)
+
+?mean # mesma coisa acima
+
+
+# pacote sos auxilia para buscar mais info sobre funções
+install.packages('sos')
+
+library(sos)
+
+# dentro do pacte sos temos a função findFn
+findFn('fread')
+
+# se não souber o nome da função
+help.search('randomForest')
+help.search('matplot')
+??matplot
+
+RSiteSearch('matplot')
+
+example('matplot')
+
+# sair
+q()
