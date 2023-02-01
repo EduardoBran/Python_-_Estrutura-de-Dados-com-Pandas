fa016b84541ae3644b7a775af65c34db80f441a2
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-02-01 14:41:14 -0300

    definindo variaveis 2

--- 03-Variaveis.R
+++ 03-Variaveis.R
@@ -20,3 +20,37 @@ var3 = c("primeiro", "segundo", "terceiro")
 var3
 
 mode(var3)
+
+# uma variável pode ser uma função
+var4 = function(x) {x+3}
+var4
+
+mode(var4)
+
+# podemos também mudar o modo do dado
+var5 = as.character(var1) # transforma valor para caractere
+var5
+mode(var5)
+
+# atribuindo valores a objetos
+x <- c(1,2,3)
+x
+x1 = c(1,2,3)
+x1
+
+c(1,2,3) -> y
+y
+
+assign("z", c(6.3,4,-2)) # função para atribuir, atribuimos o veotr para "z"
+z
+
+# verificando o valor em uma posição específica
+z[1]
+
+# verificar objetos (variaveis)
+ls()
+objects()
+
+# remover objetos (variaveis)
+rm(x)
+x
