a85c219205d2c809f99c6be2c7000987939bec78
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-02-01 15:23:49 -0300

    tipos basicos de dados - parte 2

--- 04-TiposDados.R
+++ 04-TiposDados.R
@@ -40,3 +40,57 @@ as.integer(TRUE)
 as.integer(FALSE)
 as.integer('TRUE')
 
+
+# Character
+
+char1 = 'A'
+char1
+mode(char1)
+typeof(char1)
+
+char2 = 'cientista'
+char2
+mode(char2)
+typeof(char2)
+
+char3 = c("Data", "Scicence", "Academy")
+char3
+mode(char3)
+typeof(char3)
+
+
+# Complex (tipo complexo - numero que podemos utilizar uma unidade imaginária representado pela letra i)
+
+compl = 2.5 + 4i
+compl
+mode(compl)
+typeof(compl)
+
+sqrt(-1)
+sqrt(-1+0i)
+sqrt(as.complex(-1))
+sqrt(as.complex(-2))
+
+
+# Logic (guardando resultado lógico em uma variável)
+
+x = 1; y = 2
+z = x > y
+z
+class(z)
+
+u = TRUE; v = FALSE
+class(u)
+u & v
+u | v
+!u
+
+
+# Operações com 0
+
+5 / 0
+0 / 5
+
+
+# Erro
+'Joe' / 5
