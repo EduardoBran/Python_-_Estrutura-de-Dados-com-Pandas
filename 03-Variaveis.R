9a879b765e82a17656896ee88fc7380b6ed3f201
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-02-01 14:30:22 -0300

    definindo variaveis 1

--- 03-Variaveis.R
+++ 03-Variaveis.R
@@ -1,1 +1,22 @@
-getwd()
+# criando variáveis
+var1 = 100
+var1
+
+mode(var1)
+help("mode")
+help("sqrt")
+sqrt(var1) # raiz quadrada
+
+# podemos atribuir valor de uma variável a outra variável
+var2 = var1
+var2
+
+mode(var2)
+typeof(var2)
+help("typeof")
+
+# uma variável pode ser uma lista de elementos
+var3 = c("primeiro", "segundo", "terceiro")
+var3
+
+mode(var3)
