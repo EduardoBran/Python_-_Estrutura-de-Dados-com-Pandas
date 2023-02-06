0ea053a88c97c59e3093eda75e733da071fe95cc
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-02-06 12:22:19 -0300

    exercicios resolvidos - parte 3

--- Exercicios-Cap02.R
+++ Exercicios-Cap02.R
@@ -44,9 +44,10 @@ getwd()
 
 # ---------------------------------------------------------------------------------------------------------
 
+
 # Exercício 1 - Crie um vetor com 30 números inteiros
 
-vet_ex1 <- 1:30
+vet_ex1 <- c(1:30)
 vet_ex1
 
 vet2_ex1 <- seq(1:30)
@@ -59,6 +60,7 @@ is.vector(vet2_ex1)
 
 # ---------------------------------------------------------------------------------------------------------
 
+
 # Exercício 2 - Crie uma matriz com 4 linhas e 4 colunas preenchida com números inteiros
 
 matriz_ex2 = matrix(c(1:16), nr = 4, nc = 4)
@@ -69,6 +71,7 @@ matriz2_ex2
 
 # ---------------------------------------------------------------------------------------------------------
 
+
 # Exercício 3 - Crie uma lista unindo o vetor e matriz criados anteriormente
 
 lista_ex3 = list(vet_ex1, matriz_ex2)
@@ -76,35 +79,93 @@ lista_ex3
 
 # ---------------------------------------------------------------------------------------------------------
 
+
 # Exercício 4 - Usando a função read.table() leia o arquivo do link abaixo para uma dataframe
 # http://data.princeton.edu/wws509/datasets/effort.dat
 
-df_ex4 = read.table('https://grodri.github.io/datasets/effort.dat')
+df_ex4 <- data.frame(read.table('https://grodri.github.io/datasets/effort.dat'))
 df_ex4
+
+class(df_ex4)
 
 # Exercício 5 - Transforme o dataframe anterior, em um dataframe nomeado com os seguintes labels:
 # c("config", "esfc", "chang")
 
-names(df_ex4) <- c('config', 'esfc', 'chang')
+names(df_ex4) = c('config', 'esfc', 'chang')
 df_ex4
 
 # ---------------------------------------------------------------------------------------------------------
 
-# Exercício 6 - Imprima na tela o dataframe iris, verifique quantas dimensões existem no dataframe iris e
-# imprima um resumo do dataset
 
-imprimindo_df_iris = iris
-imprimindo_df_iris
+# Exercício 6 - Imprima na tela o dataframe iris,
+# verifique quantas dimensões existem no dataframe iris e imprima um resumo do dataset
+
+iris
 View(iris)
 
 dim(iris)
-
-head(iris)
-
-summary(iris)
+summary(iris) # resumo estatístico
+str(iris) # resumo de cada tipo de dado
 
 # ---------------------------------------------------------------------------------------------------------
 
+
 # Exercício 7 - Crie um plot simples usando as duas primeiras colunas do dataframe iris
+# plot = gráfico simples
 
 plot(iris$Sepal.Length, iris$Sepal.Width)
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+# Exercício 8 - Usando a função subset, crie um novo dataframe com o conjunto de dados do dataframe iris
+# em que Sepal.Length > 7
+# Dica: consulte o help para aprender como usar a função subset()
+
+?subset
+
+df_ex8 <- subset(iris, Sepal.Length > 7)
+
+View(df_ex8)
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+# Exercício 9 (Desafio) - Crie um dataframe que seja cópia do dataframe iris e usando a função slice(),
+# divida o dataframe em um subset de 15 linhas
+# Dica 1: Você vai ter que instalar e carregar o pacote dplyr
+# Dica 2: Consulte o help para aprender como usar a função slice()
+
+install.packages('dplyr')
+
+library(dplyr)
+
+?slice
+
+df_ex9 = iris %>% slice_sample(n = 15) # retorna 15 linhas aleatórias
+df_ex9
+
+View(df_ex9)
+
+
+# solução professor (correta, retorna as primerias 15 linhas)
+
+novo_iris <- iris
+
+novo_iris = slice(novo_iris, 1:15)
+novo_iris
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+# Exercício 10 - Use a função filter no seu novo dataframe criado no item anterior e retorne apenas
+# valores em que Sepal.Length > 6
+# Dica: Use o RSiteSearch para aprender como usar a função filter
+
+RSiteSearch('filter')
+
+filter(df_ex9, Sepal.Length > 6)
+View(filter(df_ex9, Sepal.Length > 6))
+
+filter(novo_iris, Sepal.Length < 6)
+View(filter(novo_iris, Sepal.Length < 6))
