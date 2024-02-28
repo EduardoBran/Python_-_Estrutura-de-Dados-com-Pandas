b419d178838030e9383d4f2ecd7cbf69fa855c6d
EduardoBran <eduardo.ads1814@gmail.com>
2024-02-28 16:44:31 -0300

    iniciando repositorio

---
+++ 7.Estudo_de_Caso.R
@@ -0,0 +1,8 @@
+####  Big Data Real-Time Analytics com Python e Spark  ####
+
+# Configurando o diretório de trabalho
+setwd("~/Desktop/DataScience/CienciaDeDados/2.Big-Data-Real-Time-Analytics-com-Python-e-Spark/3.Manipulacao_de_Dados_em_Python_com_Pandas")
+getwd()
+
+
+#### Estudo de Caso #####
